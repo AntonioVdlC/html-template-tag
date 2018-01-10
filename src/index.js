82d5e8c802e3ce1e05090060173e1d07a2a09817
Cássio Zen <cassiozen@gmail.com>
2018-01-10 03:01:18 -0500

    Added option for skipping autoescape

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import htmlEscape from "html-es6cape";
 
 export default (literals, ...substs) => {
 	return literals.raw.reduce((acc, lit, i) => {
-		var subst = substs[i - 1];
-
+		let subst = substs[i - 1];
 		if (Array.isArray(subst)) {
 			subst = subst.join("");
+		} else if (acc.endsWith('$')) {
+			// If the interpolation is preceded by a dollar sign,
+			// substitution is considered safe and will not be escaped
+			acc = acc.slice(0, -1);
 		} else {
 			subst = htmlEscape(subst);
 		}
